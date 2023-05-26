90b1e55d42d9c2d491498782571fcd405204bf58
Janmejoy Sarkar <63224620+janmejoysarkar@users.noreply.github.com>
2023-05-26 09:38:44 +0530

    Add files via upload

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 $HOME/.config/i3/i3_monitor.sh
 dunst &
-dropbox start $
-nitrogen --restore &
 picom --experimental-backends  --config $HOME/.config/i3/picom/picom.conf &
-xautolock -time 10 -locker "systemctl suspend" -corners 0-00 -cornersize 20 -detectsleep &
+nitrogen --restore &
+systemctl --user start pulseaudio &
+#xautolock -time 10 -locker "systemctl suspend" -corners 0-00 -cornersize 20 -detectsleep &
+dropbox start &
 sleep 1 && notify-send "Welcome back, $USER"
