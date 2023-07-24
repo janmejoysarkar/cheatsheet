ffc84748f104b723d0f91452c823ef9d2a72b27d
Janmejoy Sarkar <63224620+janmejoysarkar@users.noreply.github.com>
2023-07-24 15:32:37 +0530

    Add files via upload

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -5,5 +5,8 @@ picom --experimental-backends  --config $HOME/.config/i3/picom/picom.conf &
 nitrogen --restore &
 systemctl --user start pulseaudio &
 #xautolock -time 10 -locker "systemctl suspend" -corners 0-00 -cornersize 20 -detectsleep &
+/usr/lib/x86_64-linux-gnu/libexec/org_kde_powerdevil & #starts powerdevil to manage power settings from kde settings.
+xset s noblank & #disables screensaver screenblanking
+xset s off & #disables screensaver
 dropbox start &
 sleep 1 && notify-send "Welcome back, $USER"
